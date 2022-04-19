08418f393e57140a4b935bb48dddefe48e9c2c95
reza2boyce <reza2boyce@gmail.com>
2022-04-20 01:18:52 +0430

    destructoring practiced

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World !</h1>
-      <p>this is the test page in my first react run</p>
-      <p>lorem</p>
+      <p>this text is for test</p>
+      <div id="content">{getCar(car)}</div>
     </div>
   );
 }
 
+const car = {
+  name: "Ford",
+  model: "Mustang",
+  year: 2014
+};
+
+function getCar({name, model, year}){
+  return name + " / " + model + " / " + year;
+}
+
 export default App;
+
