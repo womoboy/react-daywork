c15e6405b0b8b759b498e1210a06f4996e6481b7
reza2boyce <reza2boyce@gmail.com>
2022-04-20 23:46:54 +0430

    css with jsx

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1>test header</h1>
+      <h1 style={{color:"red",textAlign: "left"}}>test header</h1>
       <p>this is a test ! just it</p>
     </div>
   );
