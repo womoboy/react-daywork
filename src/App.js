7a7614dcbf4d58189ae231efea02ebb3f2ea8643
reza2boyce <reza2boyce@gmail.com>
2022-04-21 00:16:05 +0430

    add sass style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import './App.css';
+import './styleSass.scss'
 
 function App() {
-  const style = {
-    color: "red",
-    textAlign: "left"
-  };
+  
   return (
     <div className='App'>
-      <h1 style={style}>test header</h1>
+      <h1>test header</h1>
       <p>this is a test ! just it</p>
     </div>
   );
