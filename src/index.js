1ce9818e06cfe5b3825d256bdd34daeae45bb81b
reza2boyce <reza2boyce@gmail.com>
2022-04-23 03:12:29 +0430

    create content with react create element method and jsx

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const x = "Hello World !";
-
-const element = React.createElement(
-    'div',
-    { id: 'app', className: "container" },
-    x
-);
+//const element = React.createElement("div", {id: "app", className: "container"}, x);
+const element = <div id='app' className='contaienr'>{x}</div>;
 
 const root = document.getElementById("root");
 
