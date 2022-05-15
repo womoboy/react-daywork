45b2fcdff502a9f0e82666b1c1f13b0c5b8a11a6
reza2boyce <reza2boyce@gmail.com>
2022-05-16 01:17:55 +0430

    prevPractice practiced again

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {favoritcolor: 'Black'};
     }
 
     componentDidMount() {
         setTimeout(() => {
-            this.setState({favoritcolor: 'Red'});
+            this.setState({favoritcolor: 'Red'}); 
         }, 1000);
     }
 
     componentDidUpdate() {
-        document.getElementById('showColor').innerHTML = this.state.favoritcolor;
+        setTimeout(() => {
+            document.getElementById('showColor').innerHTML = this.state.favoritcolor;
+        },1000);
     }
-    
+
     render() {
         return (
             <>
-                <p id='showColor'>the color is : {this.state.favoritcolor}</p>
+                <p id='showColor'>the color is: {this.state.favoritcolor}</p>
             </>
         )
     }
 }
 
-export default App;
+export default App
