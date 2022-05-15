e718dbbf137e8a8646481cf7ec2b8d07d5dcc447
reza2boyce <reza2boyce@gmail.com>
2022-05-16 01:04:46 +0430

    create class component to change favoritcolor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-const App = () => {
-    return (
-        <>
-            <div>Hello World ! this is the App</div>
-        </>
-    )
+import React from 'react';
+
+class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {favoritcolor: 'Black'};
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({favoritcolor: 'Red'});
+        }, 1000);
+    }
+
+    componentDidUpdate() {
+        document.getElementById('showColor').innerHTML = this.state.favoritcolor;
+    }
+    
+    render() {
+        return (
+            <>
+                <p id='showColor'>the color is : {this.state.favoritcolor}</p>
+            </>
+        )
+    }
 }
 
 export default App;
