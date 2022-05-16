e478302dd472457a55c73f0b942d159b892cd14d
reza2boyce <reza2boyce@gmail.com>
2022-05-16 18:28:24 +0430

    prevPractice practiced again

--- src/component/testform/Testform.js
+++ src/component/testform/Testform.js
@@ -2,25 +2,26 @@ import './Testform.scss';
 import { useState } from 'react';
 
 const Testform = () => {
-    const[inputs, setInputs] = useState({});
 
-    const handleChange = (e) => {
+    const [inputs, setInputs] = useState({});
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(inputs);
+    }
+
+    const handlerChange = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-        setInputs(values => ({...values, [name]: value}));
-    }
-    
-    const submitFunc = (event) => {
-        event.preventDefault();
-
+        setInputs((values) => ({...values, [name]: value}))
     }
 
     return (
         <>
-            <form id='test-form' onSubmit={submitFunc}>
-                <h2>Test Form</h2>
-                <input type="text" placeholder='Enter your text' name='text' value={inputs.text || ''} onChange={handleChange}/>
-                <input type="submit" value="Submit" />
+            <form id='test-form' onSubmit={handleSubmit}>
+                <input type="text" placeholder="Enter first name" name="firstName" value={inputs.firstName || ''} onChange={handlerChange} />
+                <input type="text" placeholder="Enter last name" name="lastName" value={inputs.lastName || ''} onChange={handlerChange} />
+                <input type="submit" />
             </form>
         </>
     )
