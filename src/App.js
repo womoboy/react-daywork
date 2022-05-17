a9c56304cfbd312fa58c820a8b287b26bd6163cf
reza2boyce <reza2boyce@gmail.com>
2022-05-17 23:21:17 +0430

    useState hooks practiced change color when click button

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import Todos from './components/todos/Todos';
-import { useState } from 'react';
-
+import { useState } from 'react'
 
 const App = () => {
-    const [count, setCount] = useState(0);
-    const [todos, setTodos] = useState(['todos1', 'todos2']);
 
-    const increment = () => {
-        setCount((e) => e + 1);
-        
+    const [color, setColor] = useState('');
+
+    const getColor = () => {
+        setColor('Red');
     }
 
-    const getTodos = () => {
-        setTodos((values) => [...values, 'todos3', 'todos 4'])
-    }
-
-    return(
+    return (
         <>
-            <Todos todos={todos}/>
-            <hr />
-            <div>
-                count: {count}
-                <button onClick={increment} >+</button>
-                <button onClick={getTodos}>todos action</button>
-            </div>
-
+            <p>My favorit color is : {color}</p>
+            <button onClick={ getColor } >ChnageColor</button>
         </>
     )
 }
