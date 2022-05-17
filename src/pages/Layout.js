256151827e3d8147302b7157beb019f356c9c275
reza2boyce <reza2boyce@gmail.com>
2022-05-17 17:07:57 +0430

    router / Outlet practiced

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,7 +1,24 @@
+import { Outlet, Link } from 'react-router-dom';
+import './Layout.scss';
+
 const Layout = () => {
     return (
         <>
-            <p>this is the layout page</p>
+            <nav>
+                <ul>
+                    <li>
+                        <Link className='link' to="/" >Home</Link>
+                    </li>
+                    <li>
+                        <Link className='link' to="/blogs" >Blogs</Link>
+                    </li>
+                    <li>
+                        <Link className='link' to="/contact" >Contact</Link>
+                    </li> 
+                </ul>
+            </nav>
+
+            <Outlet />     
         </>
     )
 }
