bff7139d1b14958f7362069c437acf281d6feb73
reza2boyce <reza2boyce@gmail.com>
2022-05-18 16:33:07 +0430

    when apple counter change useState run

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import { useState } from 'react';
-import ShowInfo from './components/showInfo/ShowInfo';
+
+import { useState, useEffect } from 'react';
 
 const App = () => {
 
-    const [count, setCount] = useState(0);
+    const [bananaCount, setBananaCount] = useState(0);
+    const [appleCount, setAppleCount] = useState(0);
 
-    const goAction = () => {
-        setCount((val) => {
-            return val + 1;
-        })
+    const handlerBanana = () => {
+        setBananaCount((values) => values + 1);
     }
+
+    const handlerApple = () => {
+        setAppleCount((values) => values + 1);
+    }
+
+    useEffect(() => {
+        console.log('apple count work');
+    }, [appleCount]);
 
     return (
         <>
-            <p>the count is : {count} </p>
-            <button onClick={goAction} >GoAction</button>
-
-            <ShowInfo countNum={count}/>
+            <p>there is count banana count in storge : {bananaCount} </p>
+            <button onClick={ handlerBanana }>+1 banana</button>
+            <p>there is count apple count in storge : {appleCount} </p>
+            <button onClick={ handlerApple } >+1 Apple</button>
         </>
     )
 }
--- src/components/showInfo1/ShowInfo1.js
+++
@@ -1,9 +0,0 @@
-const ShowInfo1 = ({ countNum }) => {
-    return (
-        <>
-            <p>this is showInfo1 the count from app is : {countNum} </p>
-        </>
-    )
-}
-
-export default ShowInfo1;
