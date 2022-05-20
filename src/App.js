601c6923287efc979d283cd062fcfc4a697fb601
reza2boyce <reza2boyce@gmail.com>
2022-05-20 17:04:16 +0430

    use hooks useState/useContext/Router manage object properties with onclick event

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
+import React, { useState, createContext } from "react";
+
 //import router
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //import pages
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import About from './pages/About'
-import Layout from './pages/Layout'
-import Blogs from './pages/Blogs'
-import NoPage from './pages/NoPage'
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Layout from "./pages/Layout";
+import Blogs from "./pages/Blogs";
+import NoPage from "./pages/NoPage";
+
+//create context
+export const UserContext = createContext();
 
 const App = () => {
-    return (
-        <>
-            <Router>
-                <Routes>
-                    <Route path='/' element={<Layout />}>
-                        <Route index element={<Home />} />
-                        <Route path='about' element={<About />} />
-                        <Route path='blogs' element={<Blogs />} />
-                        <Route path='contact' element={<Contact />} />
-                    </Route>
-                </Routes>
-            </Router>
-        </>
-    )
-}
+  const [user, setUser] = useState({
+      firstName: 'John',
+      lastName: 'Doe'
+  });
+
+  return (
+    <>
+      <UserContext.Provider value={ { user , setUser} }>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="blogs" element={<Blogs />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="*" element={<NoPage />} />
+            </Route>
+          </Routes>
+        </Router>
+      </UserContext.Provider>
+    </>
+  );
+};
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,25 @@
+import React, { useContext } from 'react';
+import { UserContext } from '../App';
+
 const Home = () => {
+
+    const userObj = useContext(UserContext);
+
+    const handlerUser = () => {
+        userObj.setUser((prevValues) => {
+            return (
+                {...prevValues, firstName: 'Aria', lastName: 'Atabak'}
+            )
+        });
+    }
+
     return (
-        <h2>This is the Home Component</h2>
+        
+        <>
+            <h2>This is the Home Component</h2>
+            <p>the user is : { userObj.user.firstName + ' ' + userObj.user.lastName } </p>
+            <button onClick={ handlerUser }>Change User</button>
+        </>
     )
 }
 
