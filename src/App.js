e93dc980d5ef5aad55b423c8711fcdb033258784
reza2boyce <reza2boyce@gmail.com>
2022-05-21 12:16:34 +0430

    useState/ useRef

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, { useState, useEffect, useRef } from 'react';
+
 const App = () => {
+
+    const [input, setInput] = useState('');
+    const count = useRef(0);
+
+    useEffect(() => {
+        count.current += 1;
+    });
+
     return (
         <>
-            <h2>hello World</h2>
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+            <p>The count is : {count.current} </p>
         </>
     )
 }
